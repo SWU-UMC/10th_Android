f87d7ad027386e6c9dc82f05fcca364a9ef5e484
mookeunjif <mookeunjiforschool@gmail.com>
2026-03-25 20:50:08 +0900

    feat: 감정우표 구현

---
+++ Week1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //TextView에 내용 삽입
+        binding.tvMain.text = "Welcome UMC_9th!!"
+
+    }
+}
