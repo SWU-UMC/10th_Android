fa9599f5ac5f74a1646a5017b77c0dea27e82c83
Lemon0610 <92876763+Lemon0610@users.noreply.github.com>
2026-04-01 16:38:01 +0900

    fix: 1주차 과제_피드백 반영

--- Week1/app/src/main/java/com/example/week1/MainActivity.kt
+++ Week1/app/src/main/java/com/example/week1/MainActivity.kt
@@ -1,68 +1,68 @@
 package com.example.week1
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.TextView
-import android.widget.ImageView
-import android.graphics.Color
+import com.example.week1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val text1 : TextView = findViewById<TextView>(R.id.text1)
-        val text2 : TextView = findViewById<TextView>(R.id.text2)
-        val text3 : TextView = findViewById<TextView>(R.id.text3)
-        val text4 : TextView = findViewById<TextView>(R.id.text4)
-        val text5 : TextView = findViewById<TextView>(R.id.text5)
-        val good : ImageView = findViewById<ImageView>(R.id.good)
-        val happy : ImageView = findViewById<ImageView>(R.id.happy)
-        val soso : ImageView = findViewById<ImageView>(R.id.soso)
-        val bad : ImageView = findViewById<ImageView>(R.id.bad)
-        val angry : ImageView = findViewById<ImageView>(R.id.angry)
-
-        fun resetTextColors() {
-            text1.setTextColor(Color.BLACK)
-            text2.setTextColor(Color.BLACK)
-            text3.setTextColor(Color.BLACK)
-            text4.setTextColor(Color.BLACK)
-            text5.setTextColor(Color.BLACK)
-        }
-
-        good.setOnClickListener {
-            resetTextColors()
-            text1.setTextColor(Color.parseColor("#F9DC77"))
-        }
-
-        happy.setOnClickListener {
-            resetTextColors()
-            text2.setTextColor(Color.parseColor("#AEE9FE"))
-        }
-
-        soso.setOnClickListener {
-            resetTextColors()
-            text3.setTextColor(Color.parseColor("#94A5FE"))
-        }
-
-        bad.setOnClickListener {
-            resetTextColors()
-            text4.setTextColor(Color.parseColor("#77C48D"))
-        }
-
-        angry.setOnClickListener {
-            resetTextColors()
-            text5.setTextColor(Color.parseColor("#D94F49"))
-        }
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+        fun resetTextColors() {
+            binding.textgood.setTextColor(Color.BLACK)
+            binding.texthappy.setTextColor(Color.BLACK)
+            binding.textsoso.setTextColor(Color.BLACK)
+            binding.textbad.setTextColor(Color.BLACK)
+            binding.textangry.setTextColor(Color.BLACK)
+        }
 
+        binding.good.setOnClickListener {
+            resetTextColors()
+            binding.textgood.setTextColor(
+                ContextCompat.getColor(this, R.color.good)
+            )
+        }
+
+        binding.happy.setOnClickListener {
+            resetTextColors()
+            binding.texthappy.setTextColor(
+                ContextCompat.getColor(this, R.color.happy)
+            )
+        }
+
+        binding.soso.setOnClickListener {
+            resetTextColors()
+            binding.textsoso.setTextColor(
+                ContextCompat.getColor(this, R.color.soso)
+            )
+        }
+
+        binding.bad.setOnClickListener {
+            resetTextColors()
+            binding.textbad.setTextColor(
+                ContextCompat.getColor(this, R.color.bad)
+            )
+        }
+
+        binding.angry.setOnClickListener {
+            resetTextColors()
+            binding.textangry.setTextColor(
+                ContextCompat.getColor(this, R.color.angry)
+            )
+        }
+    }
 }
